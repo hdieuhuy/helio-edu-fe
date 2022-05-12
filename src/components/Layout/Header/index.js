a028e2174cf40c24ebcaf53f69304d770bd968b8
dieuhuy227 <hdieuhuy227@gmail.com>
2022-05-13 01:19:04 +0700

    implement header

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Logo from 'src/assets/images/logo.png';
 
 const Header = () => {
-  return <div className="layout__header">Header</div>;
+  return (
+    <div className="layout__header">
+      <div className="logo">
+        <img src={Logo} />
+      </div>
+
+      <div className="feature">
+        <div className="feature-item">Tìm gia sư</div>
+        <div className="feature-item">Đăng ký gia sư</div>
+        <div className="feature-item">Đăng ký gia sư</div>
+      </div>
+
+      <div className="personal">
+        <div>abc</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
