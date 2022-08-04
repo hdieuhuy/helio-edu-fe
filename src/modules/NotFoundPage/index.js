1ee9f8033198d9757a099f9d7568f5bac8511077
dieuhuy227 <hdieuhuy227@gmail.com>
2022-08-04 19:59:53 +0700

    update responsive authen page

--- src/modules/NotFoundPage/index.js
+++ src/modules/NotFoundPage/index.js
@@ -4,7 +4,7 @@ import NotFoundImage from 'src/assets/images/404.jpg';
 const NotFound = () => {
   return (
     <div className="hl-ml-not-found">
-      <div className="title">Lỗi</div>
+      <div className="title">Lỗi!</div>
 
       <div className="desc">Trang không tồn tại</div>
 
